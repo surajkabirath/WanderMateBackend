4a0f2fe08bd586789c7eb7138bd3a211bd2b88cf
surajkabirath <surajkabirath1234@gmail.com>
2024-08-06 14:18:48 +0545

    ApplicationDbContext

--- context/ApplicationDbContext.cs
+++ context/ApplicationDbContext.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using WanderMateBackend.Models;
 
 namespace WanderMateBackend.context
 {
     public class ApplicationDbContext : DbContext
     {
-          public ApplicationDbContext(DbContextOptions options) 
-        : base (options)
-        {    
+        public ApplicationDbContext(DbContextOptions options)
+      : base(options)
+        {
 
-            
+            DbSet<Hotel> hotels;
         }
     }
 }
