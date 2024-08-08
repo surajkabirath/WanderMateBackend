905fbdc4a4eb6048fbe73e015120cae29cb5acdc
surajkabirath <surajkabirath1234@gmail.com>
2024-08-08 08:54:15 +0545

    controller

--- obj/Debug/net8.0/WanderMateBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/WanderMateBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WanderMateBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ac2a360fee662f0d9543525aa077fb568c28f6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afdf8d47b2318ecc4d08cb1ef58c186f74e8906f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WanderMateBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WanderMateBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
