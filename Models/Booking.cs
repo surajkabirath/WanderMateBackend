61f8684723a00299f39810cdbed35c848f0672eb
surajkabirath <surajkabirath1234@gmail.com>
2024-08-17 19:41:28 +0545

    booking model update

---
+++ Models/Booking.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WanderMateBackend.Models
+{
+    public class Booking
+    {
+        public int bookingId {get; set;}
+        public string bookingDate {get; set;}
+
+    }
+}
--- obj/Debug/net8.0/WanderMateBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/WanderMateBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WanderMateBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94868c6ddf2972065636bd8cd67062bbde8ae2ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fadcdd40efd57e62c963e29e907ab988cf19ee4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WanderMateBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WanderMateBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
